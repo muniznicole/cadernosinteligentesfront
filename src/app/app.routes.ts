d34805fdb4f9267468e55201cd81da30562ef5c3
muniznicole <nicoleamuniz@gmail.com>
2024-04-03 06:43:25 -0300

    Create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { EstadoListComponent } from './components/estado/estado-list/estado-list.component';
+import { EstadoFormComponent } from './components/estado/estado-form/estado-form.component';
+import { estadoResolver } from './components/estado/resolver/estado-resolver';
+
+import { MunicipioListComponent } from './components/municipio/municipio-list/municipio-list.component';
+import { MunicipioFormComponent } from './components/municipio/municipio-form/municipio-form.component';
+import { municipioResolver } from './components/municipio/resolver/municipio-resolver';
+
+export const routes: Routes = [
+    { path: 'estados', component: EstadoListComponent, title: 'Lista de Estados'},
+    { path: 'estados/new', component: EstadoFormComponent, title: 'Novo Estado'},
+    { path: 'estados/edit/:id', component: EstadoFormComponent, resolve: {estado: estadoResolver}},
+
+    { path: 'municipios', component: MunicipioListComponent, title: 'Lista de Municipios'},
+    { path: 'municipios/new', component: MunicipioFormComponent, title: 'Novo Municipio'},
+    { path: 'municipios/edit/:id', component: MunicipioFormComponent, resolve: {municipio: municipioResolver}},
+];
